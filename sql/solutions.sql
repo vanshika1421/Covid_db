628c884cf2856a3ffa722187798b6a3208d5c085
vanshika <vanshika1407@gmail.com>
2026-09-16 21:46:01 +0530

    Add UC2 query

--- sql/solutions.sql
+++ sql/solutions.sql
@@ -8,3 +8,16 @@ WHERE cs.report_date = '2020-02-06'
 GROUP BY c.country_id, c.name
 ORDER BY total_confirmed DESC
 LIMIT 1;
+--UC2
+SELECT 
+    c.name AS country,
+    s.state_name AS state,
+    SUM(cs.deaths) AS total_deaths
+FROM country c
+JOIN covid_case_stats cs
+    ON c.country_id = cs.country_id
+JOIN state s
+    ON cs.state_id = s.state_id
+WHERE cs.report_date = '2020-02-06'
+GROUP BY c.name, s.state_name
+ORDER BY c.name, total_deaths DESC;
